77a269fec93b9512af15430a6b69129b468e5ae6
Surya Murali <msurya25@gmail.com>
2020-08-16 21:39:03 -0700

    Update scrapper_udf.R

--- scrapper_udf.R
+++ scrapper_udf.R
@@ -5,74 +5,143 @@ library("pacman")
 #p_load allows the user to load one or more packages as a substitute for the library function
 pacman::p_load(XML, dplyr, stringr, rvest, audio)
 
-analyseReviews <- function(prod_code, pages = 2){
+#Parse Amazon html pages for data
+amazon_scraper <- function(doc, reviewer = T, delay = 0){
   
-  # Creating a trim function to remove all white spaces
+  if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
+  pacman::p_load_gh("trinker/sentimentr")
+  pacman::p_load(RCurl, XML, dplyr, stringr, rvest, audio)
+  
+  sec = 0
+  if(delay < 0) warning("delay was less than 0: set to 0")
+  if(delay > 0) sec = max(0, delay + runif(1, -1, 1))
+  
+  #Remove all white space
   trim <- function (x) gsub("^\\s+|\\s+$", "", x)
   
-  #Amazon's URL is easy to build. Just concatenate the product code at the end
-  url <- paste0("https://www.amazon.com/dp/", prod_code)
-  #Read the HTML source of the URL and store it in doc
-  doc <- read_html(url)
-
-  #Obtain the product name. The product name is stored in the 'doc' html. '#productTitle' gives the product name
-  #Also trim the spaces and new lines
-  prod <- html_nodes(doc, "#productTitle") %>% html_text() %>% gsub("\n", "", .) %>% trim()
-
-  #Now we have got the Product name!
-  prod
-  #Source funtion is used to parse Amazon html pages for data
-  #The source has the amazon_scraper function in it
-  #It parses the data and gives the following values - prod, title, author, date, ver.purchase, format, stars, comments and helpful for each product
-  source("https://raw.githubusercontent.com/rjsaito/Just-R-Things/master/Text%20Mining/amazonscraper.R")
+  title <- doc %>%
+    html_nodes("#cm_cr-review_list .a-color-base") %>%
+    html_text()
   
-  #Initialising the reviews_all variable as NULL
-  reviews_all <- NULL
+  # author <- doc %>%
+  #  html_nodes(".review-byline .author") %>%
+  #  html_text()
   
-  for(page_num in 1:pages){
-    url <- paste0("http://www.amazon.com/product-reviews/",prod_code,"/?pageNumber=", page_num)
-    doc <- read_html(url)
-    reviews <- amazon_scraper(doc, reviewer = F, delay = 2)
-    reviews_all <- rbind(reviews_all, cbind(prod, reviews))
-  }
- 
-  #Applying the sentiment_by function to only the comments column of the reviews_all data using the 'with()' function
-  #sent_agg <- sentiment_by(reviews_all$comments). Both perform the same function
-  #sentiment_by() function returns the approximate sentiment of the reviews. It returns a datatable of element_id, sentence_id, word_count
-  #standard deviation and average sentiment for each review
-  sent_agg <- with(reviews_all, sentiment_by(comments))
+  author <- doc %>%
+    html_nodes("#cm_cr-review_list .a-profile-name") %>%
+    html_text()
   
-  #Plot the histogram of the stars / ratings given by the customers
-  #(Rating is between 0 to 5)
-  #x <- hist(reviews_all$stars)
-  with(reviews_all, hist(stars))
-
-  #Plot the histogram of the average sentiment scores for the reviews
-  #y <- hist(reviews_all$ave_sentiment)
-  with(sent_agg, hist(ave_sentiment))
-
-  #Get the mean rating of the product
-  mean(reviews_all$stars)
-
-  #Get the mean sentiment score of the product
-  #Higher the sentiment score, more positive are the reviews
-  mean(sent_agg$ave_sentiment)
-
-  #Create a subset of the top 3 reviews as 'best_reviews' based on the sentiment score
-  best_reviews <- slice(reviews_all, top_n(sent_agg, 3, ave_sentiment)$element_id)
-  best_reviews
-
-  #Highlight the top 3 reviews by sentiment polarity (positive = green; negative = pink) as an html file.
-  highlight(sentiment_by(best_reviews$comments))
-  #with(best_reviews, sentiment_by(comments)) %>% highlight()
-
-  #Create a subset of the bottom 3 reviews as 'worst_reviews' based on the sentiment score
-  worst_reviews <- slice(reviews_all, top_n(sent_agg, 3, -ave_sentiment)$element_id)
-  worst_reviews
-
-  #Highlight the bottom 3 reviews by sentiment polarity (positive = green; negative = pink) as an html file.
-  highlight(sentiment_by(worst_reviews$comments))
-  #with(worst_reviews, sentiment_by(comments)) %>% highlight()
+  date <- doc %>%
+    html_nodes("#cm_cr-review_list .review-date") %>%
+    html_text() %>% 
+    gsub(".*on ", "", .)
+  
+  ver.purchase <- doc%>%
+    html_nodes(".review-data.a-spacing-mini") %>%
+    html_text() %>%
+    grepl("Verified Purchase", .) %>%
+    as.numeric()
+  
+  format <- doc %>% 
+    html_nodes(".review-data.a-spacing-mini") %>% 
+    html_text() %>%
+    gsub("Color: |\\|.*|Verified.*", "", .)
+  #if(length(format) == 0) format <- NA
+  
+  stars <- doc %>%
+    html_nodes("#cm_cr-review_list  .review-rating") %>%
+    html_text() %>%
+    str_extract("\\d") %>%
+    as.numeric()
+  
+  comments <- doc %>%
+    html_nodes("#cm_cr-review_list .review-text") %>%
+    html_text() 
+  
+  #helpful <- doc %>%
+  #  html_nodes(".cr-vote-buttons .a-color-secondary") %>%
+  #  html_text() %>%
+  #  str_extract("[:digit:]+|One") %>%
+  #  gsub("One", "1", .) %>%
+  #  as.numeric()
+  
+  # Helpful votes number cannot be extracted from reviews with no helpful votes cast
+  helpful <- doc %>%
+    html_nodes("#cm_cr-review_list  .cr-vote-text") %>%
+    html_text() %>%
+    str_extract("[:digit:]+|One") %>%
+    gsub("One", "1", .) %>%
+    as.numeric() 
+  
+  if(reviewer == T){
+    
+    rver_url <- doc %>%
+      html_nodes(".review-byline .author") %>%
+      html_attr("href") %>%
+      gsub("/ref=cm_cr_othr_d_pdp\\?ie=UTF8", "", .) %>%
+      gsub("/gp/pdp/profile/", "", .) %>%
+      paste0("https://www.amazon.com/gp/cdp/member-reviews/",.) 
+    
+    #average rating of past 10 reviews
+    rver_avgrating_10 <- rver_url %>%
+      sapply(., function(x) {
+        read_html(x) %>%
+          html_nodes(".small span img") %>%
+          html_attr("title") %>%
+          gsub("out of.*|stars", "", .) %>%
+          as.numeric() %>%
+          mean(na.rm = T)
+      }) %>% as.numeric()
+    
+    rver_prof <- rver_url %>%
+      sapply(., function(x) 
+        read_html(x) %>%
+          html_nodes("div.small, td td td .tiny") %>%
+          html_text()
+      )
+    
+    rver_numrev <- rver_prof %>%
+      lapply(., function(x)
+        gsub("\n  Customer Reviews: |\n", "", x[1])
+      ) %>% as.numeric()
+    
+    rver_numhelpful <- rver_prof %>%
+      lapply(., function(x)
+        gsub(".*Helpful Votes:|\n", "", x[2]) %>%
+          trim()
+      ) %>% as.numeric()
+    
+    rver_rank <- rver_prof %>%
+      lapply(., function(x)
+        gsub(".*Top Reviewer Ranking:|Helpful Votes:.*|\n", "", x[2]) %>%
+          removePunctuation() %>%
+          trim()
+      ) %>% as.numeric()
+    
+    df <- data.frame(title, date, ver.purchase, format, stars, comments, helpful,
+                     rver_url, rver_avgrating_10, rver_numrev, rver_numhelpful, rver_rank, stringsAsFactors = F)
+    
+  } #else df <- data.frame(title, author, date, ver.purchase, format, stars, comments, helpful, stringsAsFactors = F)
+  # Removing 'author', 'helpful' from the dataframe. (Resolving the open issue: Error in data.frame(title, author, date, ver.purchase, format, stars,  arguments imply differing number of rows) 
+  # Amazon.com had changed the HTML code of the reviews page. Due to different HTML nodes, this script to extract author, and helpful votes did not work
+  # I added the right HTML tags in the code, but still removed them from the data frame
+  else df <- data.frame(title, date, ver.purchase, format, stars, comments, stringsAsFactors = F)
+  
+  return(df)
 }
 
-analyseReviews("B00AFTT7IW", 5)
+#Give the number of pages of reviews you want to extract 
+pages <- 10
+
+#Initialising the reviews_all variable as NULL
+reviews_all <- NULL
+
+#Extracting the first ten pages of reviews and storing it in 'reviews_all' list variable
+for(page_num in 1:pages){
+  url <- paste0("http://www.amazon.com/product-reviews/",prod_code,"/?pageNumber=", page_num)
+  doc <- read_html(url)  
+  reviews <- amazon_scraper(doc, reviewer = F, delay = 2)
+  reviews_all <- rbind(reviews_all, cbind(prod, reviews))
+}
+
+reviews_all
